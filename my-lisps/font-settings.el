cb7d65adc93b6dec4f2436a2293d9fe091dd70cc
liubin <hhdslb@gmail.com>
2010-08-26 17:52:17 +0800

    create work branch and change some person info,add windows reg install

--- my-lisps/font-settings.el
+++ my-lisps/font-settings.el
@@ -1,13 +1,16 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2010-07-13 09:01:31 Tuesday by taoshanwen>
+;; Time-stamp: <2010-08-26 16:24:29 Administrator>
 
-(when (and window-system is-after-emacs-23)
-  (require 'my-fontset-win)
-  (if mswin
-      ;; (huangq-fontset-courier 17)
-      (huangq-fontset-consolas 17)
-    ;; (huangq-fontset-dejavu 17)))
-    (huangq-fontset-fixedsys 17)))
+;; (when (and window-system is-after-emacs-23)
+;;   (require 'my-fontset-win)
+;;   (if mswin
+;;       ;; (huangq-fontset-courier 17)
+;;       (huangq-fontset-consolas 17)
+;;     ;; (huangq-fontset-dejavu 17)))
+;;     (huangq-fontset-fixedsys 17)))
+
+;; 使用新的自动字体选择
+(require 'my-fontset)
 
 (provide 'font-settings)
