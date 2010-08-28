96087bc1b45c34ae960caffff46e612df63974a9
ahei <ahei@923f481e-9afd-77b6-f6c2-ce3d611d02f2>
2010-08-28 14:38:16 +0000

    Add cedet-speedbar-settings.
    git-svn-id: http://dea.googlecode.com/svn/trunk@1288 923f481e-9afd-77b6-f6c2-ce3d611d02f2

--- my-lisps/cedet/speedbar/cedet-speedbar-settings.el
+++ my-lisps/cedet/speedbar/cedet-speedbar-settings.el
@@ -2,7 +2,7 @@
 
 ;; Author: ahei <ahei0802@gmail.com>
 ;; URL: http://code.google.com/p/dea/source/browse/trunk/my-lisps/cedet-speedbar-settings.el
-;; Time-stamp: <2010-04-19 01:06:30 Monday by ahei>
+;; Time-stamp: <2010-08-28 21:18:39 Saturday by taoshanwen>
 
 ;; This  file is free  software; you  can redistribute  it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -19,9 +19,21 @@
 ;; write  to  the Free  Software  Foundation,  Inc., 51  Franklin
 ;; Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+(eal-define-keys
+ `speedbar-key-map
+ `(("j"   speedbar-next)
+   ("k"   speedbar-prev)
+   ("o"   other-window)
+   ("m"   speedbar-toggle-line-expansion)
+   ("SPC" View-scroll-half-page-forward)
+   ("u"   View-scroll-half-page-backward)))
+
+(eal-define-keys
+ `speedbar-file-key-map
+ `(("SPC" View-scroll-half-page-forward)))
+
 (defun cedet-speedbar-settings ()
-  "Settings for `speedbar'."
-  (require 'dframe))
+  "Settings for `speedbar'.")
 
 (eval-after-load "speedbar"
   `(cedet-speedbar-settings))
